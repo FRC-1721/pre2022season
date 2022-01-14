7207ef75560d21d2ef9f1eb2925b0189c44d5dc6
Khan Simeoni <simeonikh@gmail.com>
2022-01-14 17:55:42 -0500

    I failed the last commit oops

---
+++ src/physics.py
@@ -0,0 +1,17 @@
+#PHYSICS SIMULATION OF OUR ROBOTS IS MOST LIKELY NOT POSSIBLE
+#DUE TO A LACK OF SUPPORT FOR CAN MOTORS
+
+#import wpilib
+#import wpilib.simulation
+#import hal.simulation
+#from pyfrc.physics import drivetrains
+#
+#class PhysicsEngine:
+#     
+#    def __init__(self):
+#        test_m = wpilib.simulation.PWMSim()
+#    def update_sim(self, now, tm_diff):
+#        pass
+
+
+

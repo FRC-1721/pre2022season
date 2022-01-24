544d6917c2bd2c7bac811df346845ee492df01b4
Khan Simeoni <simeonikh@gmail.com>
2022-01-24 17:34:09 -0500

    button debug movement for compatability with Linux

--- src/robot.py
+++ src/robot.py
@@ -14,7 +14,7 @@ def vectorMath(vector1, vector2, SorL):
     vector3 = math.sqrt(vector1Circle**2 + vector2Circle**2)
     angleS = math.degrees(math.acos(vector1Circle/vector3))
     angleL = 180 - angleS
-    print(angleS, " S ", angleL, " L ", vector1, " Vector1 ", vector2, " Vector2 ", vector3, " Vector3 ", vector1Circle, " V1C ", vector2Circle, " V2C ")
+    #print(angleS, " S ", angleL, " L ", vector1, " Vector1 ", vector2, " Vector2 ", vector3, " Vector3 ", vector1Circle, " V1C ", vector2Circle, " V2C ")
     #if (SorL == 0):
     #    return angleS
     #elif (SorL == 1):
@@ -24,20 +24,20 @@ def vectorMath(vector1, vector2, SorL):
 class UnnamedToaster(wpilib.TimedRobot):
     def robotInit(self):
 
-        self.driveMotor = rev.CANSparkMax(1, rev.MotorType.kBrushless)
-        self.spinMotor = rev.CANSparkMax(2, rev.MotorType.kBrushless)
+        #self.driveMotor = rev.CANSparkMax(1, rev.MotorType.kBrushless)
+        self.spinMotor = rev.CANSparkMax(1, rev.MotorType.kBrushless)
 
-        self.driveEncoder = self.driveMotor.getEncoder()
+        #self.driveEncoder = self.driveMotor.getEncoder()
         self.spinEncoder = self.spinMotor.getEncoder()
 
         self.spinPID = self.spinMotor.getPIDController()
-        self.drivePID = self.driveMotor.getPIDController()
+       # self.drivePID = self.driveMotor.getPIDController()
 
         self.joy = wpilib.Joystick(0)
 
     def teleopInit(self):
         self.spinEncoder.setPosition(0)
-        self.driveEncoder.setPosition(0)
+        #self.driveEncoder.setPosition(0)
 
         self.spinPID.setP(0.1)
         self.spinPID.setD(0.5)
@@ -45,20 +45,32 @@ class UnnamedToaster(wpilib.TimedRobot):
         self.spinPID.setI(1e-4)
         self.spinPID.setSmartMotionMaxVelocity(0.3)
 
-        self.drivePID.setP(0.2)
-        self.drivePID.setD(0.3)
-        self.drivePID.setFF(0)
-        self.drivePID.setI(1e-4)
-        self.drivePID.setSmartMotionMaxVelocity(0.3)
+        #self.drivePID.setP(0.2)
+        #self.drivePID.setD(0.3)
+        #self.drivePID.setFF(0)
+        #self.drivePID.setI(1e-4)
+        #self.drivePID.setSmartMotionMaxVelocity(0.3)
 
     def teleopPeriodic(self):
         #methods of spinning
-        #self.spinMotor.set(self.joy.getRawAxis(2)/10)
+        #self.spinMotor.set(self.joy.getRawAxis(2)/20)
         #self.spinPID.setReference(self.joy.getRawAxis(2)*5, rev.ControlType.kPosition, 0)   
+        if (self.joy.getRawButton(5) == True):
+            self.spinMotor.set(0.2)
+        if (self.joy.getRawButton(6) == True):
+            self.spinMotor.set(-0.2)
+
+        if (self.joy.getRawButton(3) == True):
+            self.spinMotor.set(0.1)
+        if (self.joy.getRawButton(4) == True):
+            self.spinMotor.set(-0.1)
+        
+        if (self.joy.getRawButton(1) == True):
+            self.spinMotor.set(0)
 
         #self.driveMotor.set(self.joy.getRawAxis(1)/4)
 
-        #Neo spins to wheel spins ratio: ~11:1
+        #Neo spins to wheel spins ratio: ~36.75:1
         if (self.joy.getRawAxis(1) < 0):
             self.joyVectorAngle = vectorMath(-self.joy.getRawAxis(1),self.joy.getRawAxis(0),0)
         elif (self.joy.getRawAxis(1) > 0):
@@ -69,11 +81,11 @@ class UnnamedToaster(wpilib.TimedRobot):
         if (self.joy.getRawAxis(0) < 0):
             self.joyVectorAngle = 360-self.joyVectorAngle
         
-        self.neoTurnRatio = 1/11
+        self.neoTurnRatio = 1/36.75
         self.joyPercent = self.joyVectorAngle / 360
         self.currentPosition = (self.spinEncoder.getPosition() % 11) / 11
 
-        print(self.neoTurnRatio,  self.joyPercent, self.currentPosition)
+        print(self.neoTurnRatio, self.joyVectorAngle,  self.joyPercent, self.currentPosition)
 
 if __name__ == "__main__":
     wpilib.run(UnnamedToaster)

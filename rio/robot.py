09f5480df5a81cee961b68473216ad2231b5723d
KenwoodFox <kenwood364@gmail.com>
2022-01-27 15:42:57 -0500

    Fix placement of python code

--- rio/robot.py
+++ rio/robot.py
@@ -2,32 +2,24 @@ import wpilib
 import wpilib.drive
 import rev
 
-
+#id 1 = drive, id 2 = turn
 class UnnamedToaster(wpilib.TimedRobot):
     def robotInit(self):
 
-        # Initialize port side drive train
-        # Port wheel
-        self.portMotorLeader = rev.CANSparkMax(2, rev.MotorType.kBrushless)
-        self.portMotorFollower = rev.CANSparkMax(1, rev.MotorType.kBrushless)
+        
+        self.drive = rev.CANSparkMax(1, rev.MotorType.kBrushless)
+        self.turn = rev.CANSparkMax(2, rev.MotorType.kBrushless)
 
-        # Starboard wheel
-        self.starboardMotorOne = rev.CANSparkMax(4, rev.MotorType.kBrushless)
-        self.starboardMotorTwo = rev.CANSparkMax(3, rev.MotorType.kBrushless)
-
-        self.portMotorFollower.follow(self.portMotorLeader)
-        self.starboardMotorTwo.follow(self.starboardMotorOne)
-
-        self.driveTrain = wpilib.drive.DifferentialDrive(
-            self.portMotorLeader, self.starboardMotorOne
-        )
+    
+        
 
         self.joy = wpilib.Joystick(0)
 
-    # Initalizes joystick to control the drivetrain
     def teleopPeriodic(self):
-        self.driveTrain.arcadeDrive(self.joy.getRawAxis(1), -self.joy.getRawAxis(2))
+        self.drive.set(self.joy.getRawAxis(1))
+        self.turn.set(self.joy.getRawAxis(0))
 
 
 if __name__ == "__main__":
     wpilib.run(UnnamedToaster)
+    

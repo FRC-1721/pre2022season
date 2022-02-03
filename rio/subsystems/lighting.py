4e89fddcf3165f73e6028c0438e136a305564135
KenwoodFox <kenwood364@gmail.com>
2022-02-03 08:18:03 -0500

    Boilerplate most of lighting subsystem

---
+++ rio/subsystems/lighting.py
@@ -0,0 +1,40 @@
+# FRC 1721
+# 2022
+
+import logging
+
+from ctre.led import CANdle, CANdleConfiguration, LEDStripType
+from commands2 import SubsystemBase
+from constants.constants import getConstants
+
+
+class Lighting(SubsystemBase):
+    """
+    This class represents the whole lighting
+    and effects subsystem on the robot.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        # Get hardware constants
+        self.constants = getConstants("robot_hardware")
+
+        # Configure CANdle module
+        self.CANdle = CANdle(self.constants["misc"]["CANdle"]["can_id"])
+
+        # Import CANdle configuration
+        CANdleConfig = CANdleConfiguration()
+        CANdleConfig.configLEDType = LEDStripType.RGB
+        CANdleConfig.BrightnessScalar = self.constants["misc"]["CANdle"]["brightness"]
+
+        # Write all settings
+        logging.info(type(CANdleConfig))
+        self.CANdle.configAllSettings(CANdleConfig)
+
+        self.CANdle.setLEDs(255, 255, 255)
+
+    def periodic(self):
+        candleError = self.CANdle.getLastError()  # Gets the last error from the CANdle
+
+        logging.error(f"Candle raised an error, code {candleError}")
